96ddbb3b2020464877661caa3709b7eadfd7bf3d
Byron Meals <bmeals87@gmail.com>
2022-07-02 00:05:58 -0600

    Added typescript code for given url image fyi udacity cant give usable urls

--- src/server.ts
+++ src/server.ts
@@ -18,15 +18,58 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // endpoint to filter an image from a public url.
   app.get('/filteredimage', async function(req, res) {
     const page_url = req.query.image_url;
-    console.log('url: ', page_url);
-    const testFilt = await filterImageFromURL(page_url)
-      .then(infoTest => console.log('infoTest: ', infoTest))
-      .catch(err => console.log('err: ', err))
-    console.log('filter image: ', testFilt)
+    if (!page_url) {
+      return res.status(400).send({message: 'page_url query missing'})
+    }
+    console.log('chopped url: ', page_url.substr(page_url.length -4));
     
-    res.status(200).send({ message: `got url: ${page_url}`})
+    console.log('url: ', page_url);
+
+    /*
+    const testFilt = filterImageFromURL(page_url)
+      .then((infoTest) => console.log('info success: ', infoTest))
+      .catch(err => console.log('err: ', err))
+    console.log('test info: ', testFilt);
+    */
+
+    try {
+      const localImageRepo = await filterImageFromURL(page_url)
+      console.log('local image repo: ', localImageRepo);
+      //localImageRepo.replace(`\\`, '/')
+      //console.log('replaced: ', localImageRepo);
+      
+      res.status(200).send({message: localImageRepo})
+
+      //const localImageRepoArray = localImageRepo.split('/')
+      //console.log('split repo: ', localImageRepoArray);
+      try {
+        deleteLocalFiles([localImageRepo])
+        console.log('image deleted: ');
+        
+      } catch (error) {
+        console.log('err deleting file');
+        
+      }
+      
+      
+    } catch (error) {
+      console.log('err of try: ', error);
+      res.status(400).send({message: 'Image was not found!'})
+      
+    }
+    
+    //console.log('local image repo outside try', localImageRepo);
+    
+    //const testFilt = await filterImageFromURL(page_url)
+    //  .then(infoTest => console.log('infoTest: ', infoTest))
+    //  .catch(err => console.log('err: ', err))
+    //console.log('filter image: ', testFilt)
+    
+    //res.status(200).send({ message: `got url: ${page_url}`})
     
   })
+
+
   // IT SHOULD
   //    1
   //    1. validate the image_url query
